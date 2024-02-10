af58678da8f92016d543899b8e6e23bbd9fcbcd7
ThiagoCancellarich <131214817+ThiagoCancellarich@users.noreply.github.com>
2024-02-10 13:19:26 -0300

    conexion DB

--- Backend/main.js
+++ Backend/main.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+const  URL = process.env.URL
+console.log(URL)
 
 // Crear una instancia de la aplicación de Express
 const app = express();
 
 // Configurar una ruta básica
+
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
 // Especificar el puerto en el que el servidor escuchará
+
 const PORT =  3000;
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor Express en funcionamiento en el puerto ${PORT}`);
-});
+mongoose.connect(URL).then(()=> {
+  console.log("Conexion a la BDD realizada con éxito")
+  app.listen(PORT, () => {
+      console.log("Servidor ejecutandose en http:localhost:", PORT);
+  })
+})
